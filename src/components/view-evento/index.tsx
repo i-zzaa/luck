8f7a4d3a700b514b4319280c0046a97c73f5c654
Andressa Novaes <isa.desenvolvimento@gmail.com>
2022-11-01 23:37:16 -0300

    feat: implementa visualizacao do evento

---
+++ src/components/view-evento/index.tsx
@@ -0,0 +1,45 @@
+import { Dialog } from 'primereact/dialog';
+import { weekDay } from '../../util/util';
+import { ButtonHeron } from '../button';
+import { Tag } from '../tag';
+
+export const ViewEvento = ({
+  evento,
+  open,
+  onClose,
+  onEdit
+}, any) => {
+  const header = (
+    <div className='flex justify-between items-center gap-8'>
+      <Tag type={evento.especialidade.nome} disabled={false}/>
+      <span>{ evento.paciente.nome }</span>
+      <div className='mt-[-0.5rem]'>
+       <ButtonHeron 
+          text= "Edit"
+          icon= 'pi pi-pencil'
+          type= 'transparent'
+          color="violet"
+          size= 'icon'
+          onClick={onEdit}
+        />
+      </div>
+    </div>
+  )
+
+  return (
+    <Dialog header={header} visible={open} onHide={onClose} breakpoints={{'960px': '80vw'}} >
+      <div>
+      <p className='flex gap-4 items-center justify-between'><span>{ evento.modalidade.nome }</span> <span>{ evento.statusEventos.nome }</span></p>
+      <br />
+
+      <p className='font-sans-serif font-bold'>{ evento.date } &bull; {`${evento.start} até ${evento.end}`}</p>
+      <p>{ evento.intervalo.nome } &bull; { evento.diasFrequencia.map((dia: number)=> weekDay[dia]).join('-')}</p>
+      <br />
+      <p className='flex gap-4 items-center'><i className='pi pi-map-marker'></i>{ evento.localidade.nome }</p>
+      <br />
+      <p className='flex gap-4 items-center'>{ evento.terapeuta.nome } <i className='pi pi-tag'> </i> { evento.funcao.nome }</p>
+      </div>
+
+    </Dialog>  
+  )
+}
