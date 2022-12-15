ce17ca9380057ecddbdb619326fc47da5c41d310
Andressa Novaes <isa.desenvolvimento@gmail.com>
2022-12-15 00:17:59 -0300

    fix: gambiarra pq o servidor nao tá usando as variaveis de ambiente

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,7 +7,7 @@ export interface ResponseSuccessProps {
 }
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || process.env.VITE_API_URL,
+  baseURL: 'https://api-multialcance.online/',
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
