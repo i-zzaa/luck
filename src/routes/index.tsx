3c6a2cb3cdc54d5fbea2f1209c1142cfbeea077f
Andressa Novaes <isa.desenvolvimento@gmail.com>
2023-01-28 23:48:32 -0300

    fix

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,11 @@
 import { useAuth } from '../contexts/auth';
-import PublicRoutes from './PublicRoutes';
-import OtherRoutes from './OtherRoutes';
 import { BrowserRouter } from 'react-router-dom';
 import { PermissionProvider } from '../contexts/permission';
 import { DropdownProvider } from '../contexts/dropDown';
+import React from 'react';
+
+const OtherRoutes = React.lazy(() => import('./OtherRoutes'));
+const PublicRoutes = React.lazy(() => import('./PublicRoutes'));
 
 function Routes() {
   const { signed } = useAuth();
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig, loadEnv } from "vite";
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
 
 export default ({ mode }) => {
   // Load app-level env vars to node-level env vars.
@@ -11,11 +11,11 @@ export default ({ mode }) => {
       rollupOptions: {
         output: {
           manualChunks(id) {
-            if (id.includes("node_modules")) {
+            if (id.includes('node_modules')) {
               return id
                 .toString()
-                .split("node_modules/")[1]
-                .split("/")[0]
+                .split('node_modules/')[1]
+                .split('/')[0]
                 .toString();
             }
           },
