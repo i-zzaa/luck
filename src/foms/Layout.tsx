88f3d0d70b336cfc02c3838e30cbbc2becc9ad69
Andressa Novaes <isa.desenvolvimento@gmail.com>
2023-01-31 22:56:13 -0300

    style: espacamento lateral

--- src/foms/Layout.tsx
+++ src/foms/Layout.tsx
@@ -5,7 +5,8 @@ interface Props {
 export const Layout = ({ children }: Props) => {
   return (
     <main className="h-screen overflow-y-auto bg-white">
-      <div className="mx-auto py-6 px-1 sm:px-8 lg:px-4 container my-8 mb-2">
+      {/* <div className="mx-auto py-6 px-1 sm:px-8 lg:px-4 container my-8 mb-2"> */}
+      <div className="py-6 px-1 sm:px-8 lg:px-4  my-8 mb-2">
         <div className="py-6">{children}</div>
       </div>
     </main>
