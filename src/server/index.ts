7cbb63f4537a6ac6d5a53aab0caf42e8e0426a8a
Andressa Novaes <isa.desenvolvimento@gmail.com>
2023-04-25 14:37:55 -0300

    fix: ajustes

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,7 +7,7 @@ export interface ResponseSuccessProps {
 }
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, //, 'https://api-multialcance.online', // import.meta.env.VITE_API_URL || process.env.VITE_API_URL,  import.meta.env.VITE_API_URL || process.env.VITE_API_URL, //
+  baseURL: 'https://api-multialcance.online', //   import.meta.env.VITE_API_URL || process.env.VITE_API_URL,  //
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './index.ts'],
   theme: {
     fontSize: {
-      xs: 8,
+      xs: 10,
       sm: 10,
       md: 12,
       lg: 18,
