d4eaa77718cc81644b98db24e519b0f769db181e
Andressa Novaes <isa.desenvolvimento@gmail.com>
2023-05-08 22:03:44 -0300

    refactory: primeiro

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,7 +7,7 @@ export interface ResponseSuccessProps {
 }
 
 export const api = axios.create({
-  baseURL: 'http://lfzqax.hospedagemelastica.com.br', //'https://api-multialcance.online', //import.meta.env.VITE_API_URL || process.env.VITE_API_URL, //
+  baseURL: 'https://lfzqax.hospedagemelastica.com.br', //'https://api-multialcance.online', //import.meta.env.VITE_API_URL || process.env.VITE_API_URL, //
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
