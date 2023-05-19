49bdb16e7b91ff6c8565020887440afa84bbf984
c1321644 Isa Andressa Novaes Menezes <c1321644@interno.bb.com.br>
2023-05-19 09:05:05 -0300

    feat: messagelist

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default ({ mode }) => {
   // Load app-level env vars to node-level env vars.
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
-    plugins: [react()],
+    plugins: [react(), reactRefresh()],
     build: {
       rollupOptions: {
         output: {
