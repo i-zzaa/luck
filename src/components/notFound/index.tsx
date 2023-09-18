15e96b12720d6939dda4a2e17c8d5f4e30e3d733
c1321644 Isa Andressa Novaes Menezes <c1321644@interno.bb.com.br>
2023-09-17 22:18:58 -0300

    fix: ajustes

--- src/components/notFound/index.tsx
+++ src/components/notFound/index.tsx
@@ -2,7 +2,7 @@ import { Title } from '../title';
 
 export function NotFound() {
   return (
-    <div className="uppercase flex items-center gap-4 m-auto mt-2">
+    <div className="uppercase flex items-center gap-4 m-auto mt-2 justify-center">
       <i className="pi pi-folder-open"></i>
       <Title color="gray">Não há itens</Title>
     </div>
