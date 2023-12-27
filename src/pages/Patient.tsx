f74ced0e2a51aa95fe675784376343009a6ca978
c1321644 Isa Andressa Novaes Menezes <c1321644@interno.bb.com.br>
2023-12-27 00:22:25 -0300

    fix: ajustes

--- src/pages/Patient.tsx
+++ src/pages/Patient.tsx
@@ -1,35 +1,56 @@
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { getList } from "../server";
-import { useForm } from "react-hook-form";
+import { useLocation, useNavigate } from "react-router-dom";
+import { create, getList } from "../server";
+import { Controller, useForm } from "react-hook-form";
 import { Tree } from "primereact/tree";
 import { Card } from "primereact/card";
 import { Timeline } from "primereact/timeline";
 import { formatdate } from "../util/util";
 import clsx from "clsx";
+import { ButtonHeron } from "../components/button";
+import { CONSTANTES_ROUTERS } from "../routes/OtherRoutes";
 
 export const Patient = () => {
   const location = useLocation();
   const { patient } = location.state;
-
   const [dropDownProgram, setDropDownProgram] = useState([])
   const [dropDownInfo, setDropDownInfo] = useState([])
+  const [programas, setProgramas ] = useState(null)
+  const navigator = useNavigate()
 
-  const [programas, setProgramas ] = useState<any[]>([])
-  const [selectedKeys, setSelectedKeys] = useState([]);
+  const {
+    handleSubmit,
+    formState: { errors },
+    control,
+    setValue,
+  } = useForm<any>();
+
 
   const customizedMarker = (item: any) => {
     return (
-        <span className={clsx('flex w-2rem h-2rem items-center p-2 rounded-full justify-center text-white border-circle z-1 shadow-1', {
-          'bg-to': item.evento.especialidade.nome.toUpperCase() === 'TO',
-          'bg-fono': item.evento.especialidade.nome.toUpperCase() === 'FONO',
-          'bg-psico': item.evento.especialidade.nome.toUpperCase() === 'PSICO',
-          'bg-black': item.evento.especialidade.nome.toUpperCase() === 'PSICOPEDAG',
-        })} >
-            <i className="pi pi-calendar"></i>
-        </span>
+      <span className={clsx('flex w-2rem h-2rem items-center p-2 rounded-full justify-center text-white border-circle z-1 shadow-1', {
+        'bg-to': item.evento.especialidade.nome.toUpperCase() === 'TO',
+        'bg-fono': item.evento.especialidade.nome.toUpperCase() === 'FONO',
+        'bg-psico': item.evento.especialidade.nome.toUpperCase() === 'PSICO',
+        'bg-black': item.evento.especialidade.nome.toUpperCase() === 'PSICOPEDAG',
+      })} >
+          <i className="pi pi-calendar"></i>
+      </span>
     );
   };
+
+  const onSubmit = async (formValue: any) => {
+    const data = await create('sessao/protocolo', {
+      formValue
+    })
+
+    navigator(`/${CONSTANTES_ROUTERS.CALENDAR}`, {
+      state: {
+        event,
+        session: data
+      }
+    });
+  }
 
   const renderTitle = (item: any) => {
     return (
@@ -42,8 +63,7 @@ export const Patient = () => {
   const customizedContent = (item: any) => {
     return (
       <Card title={renderTitle(item)} subTitle={formatdate(item.evento.dataInicio)}>
-          <p>{item.resumo}</p>
-          {/* <Button label="Read more" className="p-button-text"></Button> */}
+        <p>{item.resumo}</p>
       </Card>
     );
   };
@@ -58,6 +78,60 @@ export const Patient = () => {
     setDropDownInfo(list)
   }
 
+  const renderHeader = () => {
+    return  (
+      <div className="text-primary font-base grid justify-start m-4 p-2 leading-4"> 
+        <span className="font-bold"> Protuário </span>
+        { patient.nome } 
+        <span className="text-gray-400 font-light text-sm"> [{ patient?.responsavel }]  </span>
+      </div>
+    )
+  }
+
+  const renderContent = () => {
+    return (
+      <>
+        <Controller
+          name="programas"
+          control={control}
+          render={(field: any) => (
+          <Tree 
+            filterPlaceholder="Programas" 
+            value={dropDownProgram} 
+            selectionKeys={programas} 
+            selectionMode="checkbox" 
+            className="w-full md:w-30rem" 
+            onSelectionChange={(e: any) => {
+              setProgramas(e.value)
+
+              setValue('programas', e.value)
+              return e.value
+            }}
+          />
+        )} />
+
+        <div className="p-1 my-4">
+          <Timeline value={dropDownInfo} align="left" className="gap-2" marker={customizedMarker} content={customizedContent} />
+        </div>
+      </>
+    )
+  }
+
+  const renderFooter = () => {
+    return (
+      <div className="fixed bottom-0 w-[104vw] ml-[-0.5rem]">
+        <ButtonHeron
+          text="Salvar Protocolo"
+          icon="pi pi-play"
+          type="primary"
+          color="white"
+          size="full"
+          onClick={handleSubmit(onSubmit)}
+        />
+      </div>
+    )
+  }
+
   useEffect(() => {
     renderProgram()
     renderSession()
@@ -65,30 +139,9 @@ export const Patient = () => {
   
   return (
     <div>
-      <div className="text-primary font-base grid justify-start m-4 p-2 leading-4"> 
-      <span className="font-bold"> Protuário</span>
-     
-        { patient?.nome } 
-        <span className="text-gray-400 font-light text-sm"> [{ patient?.responsavel }]  </span>
-      
-      </div>
-
-      <div>
-        <Tree 
-          filter filterMode="strict" filterPlaceholder="Programas" 
-          value={dropDownProgram} 
-          selectionMode="checkbox" 
-          selectionKeys={selectedKeys} 
-          onSelectionChange={(e: any) => {
-            setSelectedKeys(e.value)
-          }} 
-          className="w-full md:w-30rem" 
-        />
-      </div>
-
-      <div className="p-1 my-4">
-        <Timeline value={dropDownInfo} align="left" className="gap-2" marker={customizedMarker} content={customizedContent} />
-      </div>
+      { renderHeader() }
+      { renderContent() }
+      { renderFooter() }
     </div>
   )
 }
