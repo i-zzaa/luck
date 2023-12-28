73b6dc1ee32eb7423f92164fd5410523b6355ee3
c1321644 Isa Andressa Novaes Menezes <c1321644@interno.bb.com.br>
2023-12-27 23:31:10 -0300

    fix: ajustes

--- src/pages/Sessao.tsx
+++ src/pages/Sessao.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useMemo, useState } from "react"
 import { ButtonHeron, Card, Input } from "../components"
-import { Checkbox } from "primereact/checkbox"
 import { useLocation, useNavigate } from "react-router-dom"
 import { sessionResumoFields } from "../constants/session";
 import { useToast } from "../contexts/toast";
@@ -8,7 +7,7 @@ import { CONSTANTES_ROUTERS } from "../routes/OtherRoutes";
 import { Controller, useForm } from "react-hook-form";
 import { create, getList } from "../server";
 import { formatdate } from "../util/util";
-import { Tree } from "primereact/tree";
+import { MultiStateCheckbox } from "primereact/multistatecheckbox";
 
 const fields = sessionResumoFields;
 const fieldsState: any = {};
@@ -17,7 +16,11 @@ fields.forEach((field: any) => (fieldsState[field.id] = ''));
 export const Sessao = () => {
   const navigator = useNavigate()
   const [dropDownProgram, setDropDownProgram] = useState([])
-  const [programas, setProgramas ] = useState([]) as any
+  const options = [
+    { value: true, icon: 'pi pi-check' },
+    { value: false, icon: 'pi pi-times' }
+];
+
 
   const { renderToast } = useToast();
   const location = useLocation();
@@ -39,36 +42,25 @@ export const Sessao = () => {
   }, [])
 
 
-  const onSubmitResumo = async ({ resumo }: any) => {
-    if ( resumo){
+  const onSubmitResumo = async (formValue: any) => {
+    try {
+      const payload: any =  {
+        resumo          : formValue.resumo,
+        calendarioId    : event.id,
+        pacienteId    : event.paciente.id,
+        sessao: JSON.stringify(dropDownProgram)
+      }
+
+      await create('sessao', payload)
+
+
+      // navigator(`/${CONSTANTES_ROUTERS.CALENDAR}`);
       renderToast({
         type: 'success',
         title: 'Sucesso!',
         message: 'Resumo da sessão enviado!',
         open: true,
       })
-
-
-    }  else {
-      renderToast({
-        type: 'failure',
-        title: 'Erro!',
-        message: 'Necessário preencher o resumo',
-        open: true,
-      })
-    }
-
-    try {
-      const date = await create('sessao', {
-        resumo,
-        pacienteId: event.pacient.id,
-        programas:programas,
-        calendarioId: event.id,
-        especialidadeId: event.especialidade.id,
-        terapeutaUsuarioId: event.terapeuta.id,
-      })
-
-      navigator(`/${CONSTANTES_ROUTERS.CALENDAR}`);
     } catch (error) {
       renderToast({
         type: 'failure',
@@ -82,8 +74,9 @@ export const Sessao = () => {
   const renderHeader =  useMemo(() => {
     return  (
       <div className="text-primary font-base grid justify-start m-4 p-2 leading-4"> 
-      <span className="font-bold"> Sessão </span>
-        { event.paciente.nome } 
+      <span className="font-base"> Sessão </span>
+      <span className="font-bold">   { event.paciente.nome }  </span>
+      
         <span className="text-gray-400 font-light text-sm font-inter"> { formatdate(event.dataInicio) }  </span>
       </div>
     )
@@ -91,24 +84,60 @@ export const Sessao = () => {
 
   const renderContent = () => {
     return (
-      <>
-      <Controller
-        name="atividades"
-        control={control}
-        render={() => (
-         <Tree 
-          filter filterMode="strict" filterPlaceholder="Programas" 
-          value={dropDownProgram} 
-          selectionKeys={programas} 
-          selectionMode="checkbox" 
-          className="w-full md:w-30rem" 
-          onSelectionChange={(e: any) => {
-            setProgramas(e.value)
-            setValue('atividade', e.value)
-            return e.value
-          }}
-        />
-      )} />
+      <div className="p-4">
+      {
+        dropDownProgram.map((program: any, programKey: number) => (
+          <>
+          <span className="text-md text-gray-400" key={programKey}>{ program.label }</span>
+            <div>
+              {
+                program.children.map((task: any, index: number) => (
+                  <Card key={index}>
+                    <span className="text-base text-primary">{ task.label }</span>
+                    <div className="flex gap-2 p-2">
+                       {
+                        task.loop.map((value: any, key: number)=> {
+                          const loop =  program.children[index].loop
+                          const disabled = key >= 3 &&  loop[key - 1] && loop[key - 2] && loop[ key - 3]
+
+                          return (
+                            <Controller  key={key} name="atividade"  control={control} render={({ field }) => (
+                              <div  key={field.name}  className="grid justify-center text-center text-sm text-gray-400">
+                                <label htmlFor={field.name} > { key } </label>
+                                <MultiStateCheckbox  disabled={disabled} value={value} onChange={() => {
+                                  const program: any = [...dropDownProgram]
+                                  let current =  loop[key]
+
+                                  switch (current) {
+                                    case true:
+                                      current = false
+                                      break;
+                                    case false:
+                                      current = 'no value'
+                                      break;
+                                    default:
+                                      current = true
+                                      break;
+                                  }
+                                  program[programKey].children[index].loop[key] = current
+
+                                  setDropDownProgram(program)
+
+                                }} ref={field.ref} options={options} optionValue="value"  />
+                              </div>
+                            )}
+                          />
+                          )
+                        })
+                       }
+                    </div>
+                  </Card>
+                ))
+              }
+            </div>
+          </>
+        ))
+      }
 
       {fields.map((item: any) => (
         <Input
@@ -121,7 +150,7 @@ export const Sessao = () => {
           errors={errors}
         />
       ))}
-    </>
+    </div>
     )
   }
   
